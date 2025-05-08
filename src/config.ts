278d4a611a475be3fb7696b584de76c1205c6d98
Aevarkan <aevarkan@gmail.com>
2025-05-08 19:47:08 +0800

    added config

---
+++ src/config.ts
@@ -0,0 +1,21 @@
+/**
+ * This file is part of Areas which is released under GPL-3.0.
+ * See file LICENCE or go to https://www.gnu.org/licenses/gpl-3.0.en.html for full licence details.
+ * File: config.ts
+ * Author: Aevarkan
+ */
+
+/**
+ * Configuration settings.
+ */
+export default {
+    /**
+     * The tag to check if a player should have admin permissions according to Areas.
+     * @remarks set this to undefined to not have a tag.
+     */
+    OperatorTag: "areas:admin",
+    /**
+     * Whether Areas should check
+     */
+    CheckForOp: true
+}
